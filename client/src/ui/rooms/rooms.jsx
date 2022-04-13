632786fe5cb83ed745806dd9257b306646521cab
Boogeyman <boogeymanqq@gmail.com>
2022-04-13 12:12:46 +0300

    fix room

--- client/src/ui/rooms/rooms.jsx
+++ client/src/ui/rooms/rooms.jsx
@@ -13,33 +13,51 @@ const newNavGame = navGames.filter((elem) => elem.title !== roomLink);
 export const Room = () => {
   const [cardRooms, setCardRooms] = useState(rooms);
   const [size, setSize] = useState(100);
-  const [verticalSize, setVerticalSize] = useState(100);
+  const [objSize, setObjSize] = useState(100);
   const [drag, setDrag] = useState(false);
+  const [isShow, setIsShow] = useState(true);
 
   // const [position, setPosition] = useState({ x: 100, y: 100 });
-
   const nodeRef = useRef(null);
 
   function startDrag(e, id) {
     e.preventDefault();
     setCardRooms(
-      cardRooms.map((item) => {
+      filtredRoom.map((item) => {
         if (item.id === id) {
           item.completed = false;
         }
         return item;
       })
     );
-    // console.log(completed);
   }
 
-  function stopDrag(e, completed) {
+  // function dragDrag(id) {
+  //   setCardRooms(
+  //     filtredRoom.map((item) => {
+  //       if (item.id === id) {
+  //         item.completed = false;
+  //       }
+  //       return item;
+  //     })
+  //   );
+  // }
+
+  // function clickClick(id) {
+  //   setCardRooms(
+  //     filtredRoom.map((item) => {
+  //       if (item.id === id) {
+  //         item.completed = false;
+  //       }
+  //       return item;
+  //     })
+  //   );
+  // }
+
+  function toggle(e, id) {
     e.preventDefault();
-  }
-
-  function toggle(id) {
     setCardRooms(
-      cardRooms.map((item) => {
+      filtredRoom.map((item) => {
         if (item.id === id) {
           item.completed = !item.completed;
         }
@@ -71,185 +89,183 @@ export const Room = () => {
     setDrag(false);
   }
 
+  // console.log(localStorage.getItem("login") === "wqer@wsdsa.com");
+
+  function onChange(id) {
+    setCardRooms(
+      cardRooms.map((elem) => {
+        if (elem.id === id) {
+          elem.checked = !elem.checked;
+        }
+        return elem;
+      })
+    );
+  }
+
+  function isReturn() {
+    setIsShow(true);
+    // window.location.reload();
+    setCardRooms(
+      rooms.map((elem) => {
+        if (elem.checked === true) {
+          elem.checked = false;
+        }
+        return elem;
+      })
+    );
+  }
+
+  const filtredRoom = cardRooms.filter((elem) => elem.checked === true);
+  console.log(filtredRoom);
+
   return (
     <>
-      <div className={style.navigation}>
-        <Navbar
-          className={style.navigation__navbar}
-          expand="md"
-          container="md"
-          color="light"
-        >
-          <Nav className={style.list}>
-            {newNavGame.map((elem, index) => (
-              <NavItem className={style.navigation__navitem} key={index}>
-                <Link to={elem.href}>
-                  <div className={style.navigation__navlink}>{elem.title}</div>
-                </Link>
-              </NavItem>
-            ))}
-          </Nav>
-        </Navbar>
-      </div>
-      <h2>Make a Room</h2>
-      <input
-        type="range"
-        value={size}
-        onInput={(e) => setSize(e.target.value)}
-      />
-      <br />
-      <input
-        type="range"
-        value={verticalSize}
-        onInput={(e) => setVerticalSize(e.target.value)}
-      />
-      <div className={s.room}>
-        <div
-          className={s.room15}
-          style={{ width: `${9 * size}px`, height: `${6 * verticalSize}px` }}
-        ></div>
-        <div
-          className={s.parts}
-          style={{ width: `${9 * size}px`, height: `${6 * verticalSize}px` }}
-        >
-          {cardRooms.map((item, index) => (
-            <Draggable
-              key={index}
-              nodeRef={nodeRef}
-              // handle="#imhandle"
-              bounds=""
-              axis="both"
-              onStart={(e) => startDrag(e, item.id)}
-              onStop={(e) => stopDrag(e, item.completed)}
-              // defaultPosition={{ x: 0, y: 0 }}
-              position={item.completed === true && item.defaultPos}
-            >
-              <div ref={nodeRef}>
+      {isShow ? (
+        <>
+          <div className={s.show}>
+            {cardRooms.map((elem) => (
+              <div>
                 <img
-                  // onMouseMove={(e) => startDrag(e, item.id)}
-                  style={{
-                    width: `${1.5 * size}px`,
-                    height: `${1.5 * size}px`,
-                  }}
-                  // id="imhandle"
-                  src={item.url}
+                  style={{ width: "150px", height: "150px" }}
+                  src={elem.url}
                   alt=""
-                  className={s.room__body}
                 />
-                <button className={s.btn} onClick={() => toggle(item.id)}>
-                  &times;
-                </button>
+                <input type="checkbox" onChange={() => onChange(elem.id)} />
               </div>
-            </Draggable>
-          ))}
-        </div>
-        <div>
-          {drag ? (
-            <div
-              onDragStart={(e) => dragStartHandler(e)}
-              onDragLeave={(e) => dragLeaveHandler(e)}
-              onDragOver={(e) => dragStartHandler(e)}
-              onDrop={(e) => dropHandler(e)}
-              className={s.drop__area}
+            ))}
+          </div>
+          <button
+            className={s.show__btn}
+            type="button"
+            onClick={() => setIsShow(false)}
+          >
+            Продолжить
+          </button>
+          <div>
+            {drag ? (
+              <div
+                onDragStart={(e) => dragStartHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragOver={(e) => dragStartHandler(e)}
+                onDrop={(e) => dropHandler(e)}
+                className={s.drop__area}
+              >
+                Отпусти файлы
+              </div>
+            ) : (
+              <div
+                onDragStart={(e) => dragStartHandler(e)}
+                onDragLeave={(e) => dragLeaveHandler(e)}
+                onDragOver={(e) => dragStartHandler(e)}
+                className={s.drop__area}
+              >
+                Перенеси
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className={style.navigation}>
+            <Navbar
+              className={style.navigation__navbar}
+              expand="md"
+              container="md"
+              color="light"
             >
-              Отпусти файлы
-            </div>
-          ) : (
+              <Nav className={style.list}>
+                {newNavGame.map((elem, index) => (
+                  <NavItem className={style.navigation__navitem} key={index}>
+                    <Link to={elem.href}>
+                      <div className={style.navigation__navlink}>
+                        {elem.title}
+                      </div>
+                    </Link>
+                  </NavItem>
+                ))}
+              </Nav>
+            </Navbar>
+          </div>
+          <h2>Make a Room</h2>
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <label>
+              {size}
+              <br />
+              <input
+                type="range"
+                value={size}
+                onInput={(e) => setSize(e.target.value)}
+              />
+            </label>
+            <label>
+              {objSize}
+              <br />
+              <input
+                type="range"
+                value={objSize}
+                onInput={(e) => setObjSize(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className={s.room}>
             <div
-              onDragStart={(e) => dragStartHandler(e)}
-              onDragLeave={(e) => dragLeaveHandler(e)}
-              onDragOver={(e) => dragStartHandler(e)}
-              className={s.drop__area}
+              className={s.room15}
+              style={{ width: `${9 * size}px`, height: `${6 * size}px` }}
+            ></div>
+            <div
+              className={s.parts}
+              style={{
+                width: `${9 * objSize}px`,
+                height: `${6 * objSize}px`,
+              }}
             >
-              Перенеси
+              {filtredRoom.map((item, index) => (
+                <Draggable
+                  key={index}
+                  nodeRef={nodeRef}
+                  // handle="#imhandle"
+                  bounds=""
+                  axis="both"
+                  // onDrag={() => dragDrag(item.id)}
+                  onStart={(e) => startDrag(e, item.id)}
+                  // onStop={(e) => stopDrag(e, item.id)}
+                  // defaultPosition={{ x: 0, y: 0 }}
+                  position={item.completed && item.defaultPos}
+                >
+                  <div ref={nodeRef}>
+                    <img
+                      // onClick={() => clickClick(item.id)}
+                      style={{
+                        width: `${1.5 * objSize}px`,
+                        height: `${1.5 * objSize}px`,
+                      }}
+                      // id="imhandle"
+                      src={item.url}
+                      alt=""
+                      className={s.room__body}
+                    />
+                    {localStorage.getItem("login") === "wqer@wsdsa.com" && (
+                      <button
+                        className={s.btn}
+                        onClick={(e) => toggle(e, item.id)}
+                      >
+                        &times;
+                      </button>
+                    )}
+                  </div>
+                </Draggable>
+              ))}
             </div>
-          )}
-        </div>
-      </div>
+          </div>
+          <button
+            className={s.show__btn}
+            type="button"
+            onClick={() => isReturn()}
+          >
+            Назад
+          </button>
+        </>
+      )}
     </>
   );
 };
-
-// const [currentRoom, setCurrentRoom] = useState(null);
-// const [currentItem, setCurrentItem] = useState(null);
-
-// function dragOverHandler(e) {
-//   e.preventDefault();
-// }
-
-// function dragLeaveHandler(e) {}
-
-// function dragStartHandler(e, room, item) {
-//   setCurrentRoom(room);
-//   setCurrentItem(item);
-// }
-
-// function dragEndHandler(e) {}
-
-// function dropHandler(e, room, item) {
-//   e.preventDefault();
-//   const currentIndex = currentRoom.items.indexOf(currentItem);
-//   currentRoom.items.splice(currentIndex, 1);
-
-//   const dropIndex = room.items.indexOf(item);
-//   room.items.splice(dropIndex + 1, 0, currentItem);
-//   setCardRooms(
-//     room.map((b) => {
-//       if (b.id === room.id) {
-//         return room;
-//       }
-//       if (b.id === currentRoom.id) {
-//         return currentRoom;
-//       }
-//       return b;
-//     })
-//   );
-// }
-
-// function dropCardHandler(e, room) {
-//   room.items.push(currentItem);
-//   const currentIndex = currentRoom.items.indexOf(currentItem);
-//   currentRoom.items.splice(currentIndex, 1);
-//   setCardRooms(
-//     room.map((b) => {
-//       if (b.id === room.id) {
-//         return room;
-//       }
-//       if (b.id === currentRoom.id) {
-//         return currentRoom;
-//       }
-//       return b;
-//     })
-//   );
-// }
-
-// return (
-//   <div className={s.room}>
-//     {cardRooms.map((room) => (
-//       <div
-//         onDragOver={(e) => dragOverHandler(e)}
-//         onDrop={(e) => dropCardHandler(e, room)}
-//         className={s.board}
-//       >
-//         <div>
-//           {room.items.map((item) => (
-//             <>
-//               <img
-//                 onDragOver={(e) => dragOverHandler(e)}
-//                 onDragLeave={(e) => dragLeaveHandler(e)}
-//                 onDragStart={(e) => dragStartHandler(e, room, item)}
-//                 onDragEnd={(e) => dragEndHandler(e)}
-//                 onDrop={(e) => dropHandler(e, room, item)}
-//                 draggable={item.id === 1 ? false : true}
-//                 src={item.url}
-//                 alt=""
-//               />
-//               <button>&times;</button>
-//             </>
-//           ))}
-//         </div>
-//       </div>
-//     ))}
-//   </div>
-// );
-// };
