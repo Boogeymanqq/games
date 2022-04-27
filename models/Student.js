3b8258b781ca1cd1c7053ce170757ee375095078
Anatoly <anatolykostarev@gmail.com>
2022-04-27 15:58:49 +0300

    Add mongoose

--- models/Student.js
+++ models/Student.js
@@ -1,10 +1,10 @@
-
+const { Schema, model, Types } = require("mongoose");
 
 const schema = new Schema({
   surname: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  teacher: [{ type: Types.ObjectId, hef: 'Teacher'}]
+  teacher: [{ type: Types.ObjectId, hef: "Teacher" }],
 });
 
 module.exports = model("Student", schema);
