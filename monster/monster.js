f9d19083c94420fe819ab10cc3500aae5d2a2aa5
Artyom <42605576+bobarikin@users.noreply.github.com>
2022-05-07 16:12:15 +0300

    fix src path

--- monster/monster.js
+++ monster/monster.js
@@ -3,12 +3,17 @@ const fs = require("fs");
 const MonsterPart = require("../models/MonsterPart");
 
 module.exports = getFiles = async () => {
-  const setMonsterPart = (file) =>
-    new MonsterPart({
-      url: path.join("http://localhost:5000", "monster", "img", file).split("\\").join("/"),
+  const setMonsterPart = (file) => {
+    let str = path.join("http:", "localhost:5000", "monster", "img", file).split('\\');
+    str.splice(1, 0, '');
+    str = str.join('/');
+
+    return new MonsterPart({
+      url: str,
       position: { x: 0, y: 0 },
       isChecked: false,
     });
+  };
 
   try {
     const files = fs.readdirSync(path.join(__dirname, "img"), "utf8");
