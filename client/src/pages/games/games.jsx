2ffc06e755ac63a43d9a157a560aac03217efaac
Boogeyman <boogeymanqq@gmail.com>
2022-05-11 02:35:00 +0300

    add navigation

--- client/src/pages/games/games.jsx
+++ client/src/pages/games/games.jsx
@@ -9,84 +9,92 @@ import {
   Typography,
   Container,
 } from "@mui/material";
+import { NavigationTeacher } from "../teacher/navigationTeacher";
 
 export const Games = () => {
   return (
-    <Container
-      sx={{ display: "flex", justifyContent: "space-between", mt: 10 }}
-    >
-      <Card sx={{ maxWidth: 325, display: "flex", flexDirection: "column" }}>
-        <CardMedia
-          component="img"
-          alt=""
-          image="/img/monster/img/monster.webp"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Make a Monster
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Make your own funny Monster
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to="/monster">
-            <Button size="medium">Let's Play</Button>
-          </Link>
-        </CardActions>
-      </Card>
-      <Card
-        sx={{
-          maxWidth: 325,
-          display: "flex",
-          flexDirection: "column",
-        }}
+    <>
+      <NavigationTeacher />
+      <Container
+        sx={{ display: "flex", justifyContent: "space-between", mt: 10 }}
       >
-        <CardMedia
+        <Card sx={{ maxWidth: 325, display: "flex", flexDirection: "column" }}>
+          <CardMedia
+            component="img"
+            alt=""
+            image="/img/monster/img/monster.webp"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Make a Monster
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Make your own funny Monster
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to="/monster">
+              <Button size="medium">Let's Play</Button>
+            </Link>
+          </CardActions>
+        </Card>
+        <Card
           sx={{
-            minWidth: 250,
-            maxHeight: 240,
-            transform: "rotate(90deg)",
-            mt: 0,
+            maxWidth: 325,
+            display: "flex",
+            flexDirection: "column",
           }}
-          component="img"
-          alt=""
-          image="/img/cards.png"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Cards
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Make your own Cards
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to="/cards">
-            <Button size="medium">Let's Play</Button>
-          </Link>
-        </CardActions>
-      </Card>
-      <Card sx={{ maxWidth: 325, display: "flex", flexDirection: "column" }}>
-        <CardMedia component="img" alt="" image="/img/room/maxresdefault.jpg" />
-        <CardContent sx={{ mt: "auto" }}>
-          <Typography gutterBottom variant="h5" component="p">
-            Room
-          </Typography>
-          <Typography
-            sx={{ flex: "0 0 auto" }}
-            variant="body2"
-            color="text.secondary"
-          >
-            Make your own Room
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Link to="/room">
-            <Button size="medium">Let's Play</Button>
-          </Link>
-        </CardActions>
-      </Card>
-    </Container>
+        >
+          <CardMedia
+            sx={{
+              minWidth: 250,
+              maxHeight: 240,
+              transform: "rotate(90deg)",
+              mt: 0,
+            }}
+            component="img"
+            alt=""
+            image="/img/cards.png"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Cards
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Make your own Cards
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to="/cards">
+              <Button size="medium">Let's Play</Button>
+            </Link>
+          </CardActions>
+        </Card>
+        <Card sx={{ maxWidth: 325, display: "flex", flexDirection: "column" }}>
+          <CardMedia
+            component="img"
+            alt=""
+            image="/img/room/maxresdefault.jpg"
+          />
+          <CardContent sx={{ mt: "auto" }}>
+            <Typography gutterBottom variant="h5" component="p">
+              Room
+            </Typography>
+            <Typography
+              sx={{ flex: "0 0 auto" }}
+              variant="body2"
+              color="text.secondary"
+            >
+              Make your own Room
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Link to="/room">
+              <Button size="medium">Let's Play</Button>
+            </Link>
+          </CardActions>
+        </Card>
+      </Container>
+    </>
   );
 };
