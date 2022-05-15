26db7c9ac1774e2edb81c7e848738a0d7b168e81
Boogeyman <boogeymanqq@gmail.com>
2022-05-15 18:44:44 +0300

    completed monster on technical task

--- client/src/ui/monster/monster.jsx
+++ client/src/ui/monster/monster.jsx
@@ -8,18 +8,22 @@ const monsterLink = "Монстер";
 const newNavGame = navGames.filter((elem) => elem.title !== monsterLink);
 
 export const Monster = () => {
+  const [cardNewMonster, setCardNewMonster] = useState([]);
+  const [secondMonster, setSecondMonster] = useState([]);
   const [cardMonster, setCardMonster] = useState([]);
+
+  const [showFolder, setShowFolder] = useState(true);
+  const [showApi, setShowApi] = useState(false);
+
   const [size, setSize] = useState(100);
   const [objSize, setObjSize] = useState(100);
   const [objBoxSize, setObjBoxSize] = useState(100);
-  const [drag, setDrag] = useState(false);
-  const [isShow, setIsShow] = useState(true);
 
   const nodeRef = useRef(null);
 
   useEffect(() => {
     async function getMonster() {
-      const url = "api/monster/monsterparts";
+      const url = "api/monster/dir/monsterparts";
       const response = await fetch(url, {
         method: "GET",
         headers: {
@@ -27,7 +31,7 @@ export const Monster = () => {
         },
       });
       const data = await response.json();
-      setCardMonster(data);
+      setCardNewMonster(data);
     }
     getMonster();
   }, []);
@@ -39,7 +43,7 @@ export const Monster = () => {
   function stopDrag(e, data, id) {
     e.preventDefault();
     setCardMonster(
-      filtredMonster.map((item) => {
+      monstrik.map((item) => {
         if (item._id === id) {
           item.position = { x: data.x, y: data.y };
         }
@@ -50,7 +54,7 @@ export const Monster = () => {
 
   function toggle(id) {
     setCardMonster(
-      filtredMonster.map((item) => {
+      monstrik.map((item) => {
         if (item._id === id) {
           item.position = { x: 0, y: 0 };
         }
@@ -59,32 +63,22 @@ export const Monster = () => {
     );
   }
 
-  function dragStartHandler(e) {
-    e.preventDefault();
-    setDrag(true);
-  }
-
-  function dropHandler(e) {
-    e.preventDefault();
-    let files = [...e.dataTransfer.files];
-    const obj = { ...files };
-    console.log(obj);
-    // setCardRooms(monster.push(obj.File.name));
-    console.log(cardMonster);
-    const formData = new FormData();
-    formData.append("file", files[0]);
-    setDrag(false);
-    console.log(files);
-  }
-
-  function dragLeaveHandler(e) {
-    e.preventDefault();
-    setDrag(false);
+  function changeHandler(id) {
+    setCardNewMonster(
+      cardNewMonster.map((elem) => {
+        if (elem._id === id) {
+          setShowFolder(false);
+          setSecondMonster(elem.img);
+          // console.log(secondMonster);
+        }
+        return elem;
+      })
+    );
   }
 
   function onChange(id) {
-    setCardMonster(
-      cardMonster.map((elem) => {
+    setSecondMonster(
+      secondMonster.map((elem) => {
         if (elem._id === id) {
           elem.isChecked = !elem.isChecked;
         }
@@ -93,36 +87,79 @@ export const Monster = () => {
     );
   }
 
-  function isReturn() {
-    setIsShow(true);
-    window.location.reload();
-    // setCardMonster(
-    //   cardMonster.map((elem) => {
-    //     if (elem.isChecked === true) {
-    //       elem.isChecked = false;
-    //     }
-    //     return elem;
-    //   })
-    // );
-  }
-
-  const filtredMonster = cardMonster.filter((elem) => elem.isChecked === true);
-  // console.log(filtredMonster);
+  const filtredMonster = secondMonster.filter((elem) =>
+    elem.isChecked === true ? cardMonster.push(elem) : null
+  );
+  const monstrik = [...new Set(cardMonster)];
+  // console.log(monstrik);
 
   return (
     <>
-      {isShow ? (
-        <>
-          <NavigationGames newNavGame={newNavGame} />
-          <h1 style={{ textAlign: "center", marginTop: "30px" }}>
-            Choose a subject
-          </h1>
-          <div className={s.show}>
-            {cardMonster.map((elem, index) => (
-              <div key={index}>
+      <NavigationGames newNavGame={newNavGame} />
+      <h2 className={s.title}>Make a Monster</h2>
+      <div
+        style={{
+          width: "1300px",
+          margin: "0 auto 60px",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <label>
+          {size}
+          <br />
+          <input
+            type="range"
+            value={size}
+            onInput={(e) => setSize(e.target.value)}
+          />
+        </label>
+        <label>
+          {objSize}
+          <br />
+          <input
+            type="range"
+            value={objSize}
+            onInput={(e) => setObjSize(e.target.value)}
+          />
+        </label>
+        <label>
+          {objBoxSize}
+          <br />
+          <input
+            type="range"
+            value={objBoxSize}
+            onInput={(e) => setObjBoxSize(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className={s.room}>
+        <div
+          className={s.room15}
+          style={{ width: `${4.5 * size}px`, height: `${6 * size}px` }}
+        ></div>
+        <div
+          className={s.parts}
+          style={{
+            width: `${6 * objBoxSize}px`,
+            height: `${6 * objBoxSize}px`,
+          }}
+        >
+          {monstrik.map((item, index) => (
+            <Draggable
+              key={index}
+              nodeRef={nodeRef}
+              // bounds="parent"
+              axis="both"
+              // onDrag={(e) => dragDrag(e, item.id)}
+              onStart={(e) => startDrag(e)}
+              onStop={(e, data) => stopDrag(e, data, item._id)}
+              position={item.position}
+            >
+              <div ref={nodeRef}>
                 <img
                   style={
-                    elem.url.includes("body")
+                    item.url.includes("body")
                       ? {
                           width: `${0.6 * objSize}px`,
                           height: `${1.5 * objSize}px`,
@@ -132,109 +169,48 @@ export const Monster = () => {
                           height: `${0.8 * objSize}px`,
                         }
                   }
-                  src={elem.url}
+                  src={item.url}
                   alt=""
+                  className={s.room__body}
                 />
-                <input type="checkbox" onChange={() => onChange(elem._id)} />
+                <button className={s.btn} onClick={() => toggle(item._id)}>
+                  &times;
+                </button>
               </div>
-            ))}
-          </div>
-          <button
-            className={s.show__btn}
-            type="button"
-            onClick={() => setIsShow(false)}
-          >
-            Продолжить
-          </button>
-          <div>
-            {drag ? (
+            </Draggable>
+          ))}
+          <button onClick={() => setShowApi(!showApi)}>изменить</button>
+        </div>
+        {showApi && (
+          <>
+            {showFolder === true ? (
               <div
-                onDragStart={(e) => dragStartHandler(e)}
-                onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragOver={(e) => dragStartHandler(e)}
-                onDrop={(e) => dropHandler(e)}
-                className={s.drop__area}
+                className={s.show}
+                style={{
+                  gridColumn: "2 / 3",
+                  width: "600px",
+                  height: "600px",
+                  border: "2px solid rgba(210, 18, 73, 0.9)",
+                }}
               >
-                Отпусти файлы
+                {cardNewMonster.map((elem, index) => (
+                  <div
+                    onClick={() => changeHandler(elem._id)}
+                    className={s.monster__folder}
+                    key={index}
+                  >
+                    {elem.dir}
+                  </div>
+                ))}
+                <button onClick={() => setShowApi(!showApi)}>закрыть</button>
               </div>
             ) : (
-              <div
-                onDragStart={(e) => dragStartHandler(e)}
-                onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragOver={(e) => dragStartHandler(e)}
-                className={s.drop__area}
-              >
-                Перенеси
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <>
-          <h2 className={s.title}>Make a Monster</h2>
-          <div
-            style={{
-              width: "1300px",
-              margin: "0 auto 60px",
-              display: "flex",
-              justifyContent: "space-around",
-            }}
-          >
-            <label>
-              {size}
-              <br />
-              <input
-                type="range"
-                value={size}
-                onInput={(e) => setSize(e.target.value)}
-              />
-            </label>
-            <label>
-              {objSize}
-              <br />
-              <input
-                type="range"
-                value={objSize}
-                onInput={(e) => setObjSize(e.target.value)}
-              />
-            </label>
-            <label>
-              {objBoxSize}
-              <br />
-              <input
-                type="range"
-                value={objBoxSize}
-                onInput={(e) => setObjBoxSize(e.target.value)}
-              />
-            </label>
-          </div>
-          <div className={s.room}>
-            <div
-              className={s.room15}
-              style={{ width: `${4.5 * size}px`, height: `${6 * size}px` }}
-            ></div>
-            <div
-              className={s.parts}
-              style={{
-                width: `${6 * objBoxSize}px`,
-                height: `${6 * objBoxSize}px`,
-              }}
-            >
-              {filtredMonster.map((item, index) => (
-                <Draggable
-                  key={index}
-                  nodeRef={nodeRef}
-                  // bounds=""
-                  axis="both"
-                  // onDrag={(e) => dragDrag(e, item.id)}
-                  onStart={(e) => startDrag(e)}
-                  onStop={(e, data) => stopDrag(e, data, item._id)}
-                  position={item.position}
-                >
-                  <div ref={nodeRef}>
+              <div className={s.show}>
+                {secondMonster.map((elem, index) => (
+                  <div key={index}>
                     <img
                       style={
-                        item.url.includes("body")
+                        elem.url.includes("body")
                           ? {
                               width: `${0.6 * objSize}px`,
                               height: `${1.5 * objSize}px`,
@@ -244,27 +220,21 @@ export const Monster = () => {
                               height: `${0.8 * objSize}px`,
                             }
                       }
-                      src={item.url}
+                      src={elem.url}
                       alt=""
-                      className={s.room__body}
                     />
-                    <button className={s.btn} onClick={() => toggle(item._id)}>
-                      &times;
-                    </button>
+                    <input
+                      type="checkbox"
+                      onChange={() => onChange(elem._id)}
+                    />
                   </div>
-                </Draggable>
-              ))}
-            </div>
-          </div>
-          <button
-            className={s.show__btn}
-            type="button"
-            onClick={() => isReturn()}
-          >
-            Назад
-          </button>
-        </>
-      )}
+                ))}
+                <button onClick={() => setShowFolder(true)}>Назад</button>
+              </div>
+            )}
+          </>
+        )}
+      </div>
     </>
   );
 };
