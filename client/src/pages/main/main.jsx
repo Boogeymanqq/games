a97ba9d9be709d1bb590968478d51e6d268feb06
Anatoly <anatolykostarev@gmail.com>
2022-06-01 14:34:09 +0300

    Add Header, Navigation components into Main page

--- client/src/pages/main/main.jsx
+++ client/src/pages/main/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Navigation } from "../../ui/navigation/navigation";
+import { Header } from "../../components/header/header";
+import { Navigation } from "../../components/navigation/navigation";
 import s from "./main.module.css";
 
 export function Main() {
   return (
     <div className={s.main}>
+      <Header />
       <Navigation />
     </div>
   );
