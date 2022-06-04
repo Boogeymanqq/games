8faca03def0a2cb26dbfac1d8057deb5cd8bb3dd
Anatoly <anatolykostarev@gmail.com>
2022-06-04 10:38:51 +0300

    Fix routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,37 +1,47 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Main } from "./pages/main/main";
-import { Platform } from "./pages/platform/platform";
-import { Signin } from "./pages/sign/signin";
-import { Login } from "./pages/login/login";
-import { Contacts } from "./pages/contacts/contacts";
-import { Games } from "./pages/games/games";
-import { Monster } from "./ui/monster/monster";
-import { Cards } from "./ui/cards/cards";
-import { Room } from "./ui/rooms/rooms";
-import { Teacher } from "./pages/teacher/teacher";
-import { ChildrenPage } from "./pages/childrenPage/childrenPage";
-import { StudentList } from "./pages/teacher/students/studentList";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import { Main } from "./pages/main/main";
+// import { Platform } from "./pages/platform/platform";
+// import { Signin } from "./pages/sign/signin";
+// import { Login } from "./pages/login/login";
+// import { Contacts } from "./pages/contacts/contacts";
+// import { Games } from "./pages/games/games";
+// import { Monster } from "./ui/monster/monster";
+// import { Cards } from "./ui/cards/cards";
+// import { Room } from "./ui/rooms/rooms";
+// import { Teacher } from "./pages/teacher/teacher";
+// import { ChildrenPage } from "./pages/childrenPage/childrenPage";
+// import { StudentList } from "./pages/teacher/students/studentList";
+// import { io } from "socket.io-client";
 
-export const useRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/platform" element={<Platform caption="О платформе" />} />
-      <Route path="/signin" element={<Signin caption="Регистрация" />} />
-      <Route path="/login" element={<Login caption="Войти" />} />
-      <Route path="/contacts" element={<Contacts />} />
-      <Route path="/teacher/games" element={<Games />} />
-      <Route path="/monster" element={<Monster />} />
-      <Route path="/cards" element={<Cards />} />
-      {/* <Route path="/checkroom" element={<CheckRoom />} /> */}
-      <Route path="/room" element={<Room />} />
-      <Route
-        path="/teacher"
-        element={<Teacher caption="Регистрация ученика" />}
-      />
-      <Route path="/teacher/studentlist" element={<StudentList />} />
-      <Route path="/childrenpage" element={<ChildrenPage />} />
-    </Routes>
-  );
-};
+// export const useRoutes = () => {
+//   const openSocket = () => {
+//     var socket = io("http://localhost:5000/");
+//     socket.on("chat message", (msg) => {
+//       console.log("i am monster " + msg);
+//     });
+//   };
+
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Main />} />
+//       <Route path="/platform" element={<Platform caption="О платформе" />} />
+//       <Route path="/signin" element={<Signin caption="Регистрация" />} />
+//       <Route path="/login" element={<Login caption="Войти" />} />
+//       <Route path="/contacts" element={<Contacts />} />
+//       <Route path="/teacher/games" element={<Games />} />
+//       <Route path="/monster" element={<Monster openSocket={openSocket} />} />
+//       <Route path="/cards" element={<Cards />} />
+//       {/* <Route path="/checkroom" element={<CheckRoom />} /> */}
+//       <Route path="/room" element={<Room />} />
+//       <Route
+//         path="/teacher"
+//         element={
+//           <Teacher caption="Регистрация ученика" openSocket={openSocket} />
+//         }
+//       />
+//       <Route path="/teacher/studentlist" element={<StudentList />} />
+//       <Route path="/childrenpage" element={<ChildrenPage />} />
+//     </Routes>
+//   );
+// };
