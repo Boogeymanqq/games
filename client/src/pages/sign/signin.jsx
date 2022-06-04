8dca46b94250d28307347c41057f179f55aa9cc5
Anatoly <anatolykostarev@gmail.com>
2022-06-04 15:37:25 +0300

    Delete Link router

--- client/src/pages/sign/signin.jsx
+++ client/src/pages/sign/signin.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Pagelogo } from "../../ui/pageLogo/pageLogo";
 import { Label } from "../../ui/label/label";
 import { Burger } from "../../ui/burger/burger";
