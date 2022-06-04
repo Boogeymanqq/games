6052eafaa374d10d6150c41c383d158ab26cb6b2
Anatoly <anatolykostarev@gmail.com>
2022-06-05 00:10:46 +0300

    Fix AlertInfo styles

--- client/src/ui/alert/alert.jsx
+++ client/src/ui/alert/alert.jsx
@@ -3,7 +3,9 @@ import { Alert, Container } from "@mui/material";
 export function AlertInfo({ title, type = "error" }) {
   return (
     <Container maxWidth="sm">
-      <Alert severity={type}>{title}</Alert>
+      <Alert severity={type} sx={{ margin: "0 auto", maxWidth: "423px" }}>
+        {title}
+      </Alert>
     </Container>
   );
 }
