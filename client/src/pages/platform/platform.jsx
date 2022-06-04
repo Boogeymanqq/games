d742402b57a3b45b9e8bf934016e525e7d4ec1f9
Anatoly <anatolykostarev@gmail.com>
2022-06-05 00:12:20 +0300

    Add icons for Platform component

--- client/src/pages/platform/platform.jsx
+++ client/src/pages/platform/platform.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Headerpage } from "../../components/header-page/header-page";
 import smile from "./img/icon-smile.svg";
+import heart from "./img/icon-heart.svg";
+import lightning from "./img/icon-lightning.svg";
+import star from "./img/icon-star.svg";
 import s from "./platform.module.css";
 
 export const Platform = ({ caption }) => {
@@ -22,6 +25,11 @@ export const Platform = ({ caption }) => {
             учебника можно легко интегрировать в игру, используя шаблоны и
             готовую базу упражнений.
           </p>
+        </div>
+        <div className={s.images}>
+          <img src={heart} alt="icon" width="120" height="104" />
+          <img src={lightning} alt="icon" width="100" height="139" />
+          <img src={star} alt="icon" width="96" height="92" />
         </div>
       </main>
     </>
