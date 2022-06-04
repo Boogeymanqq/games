7a47cef09836056f7c73a1c2574b24429b21870c
Anatoly <anatolykostarev@gmail.com>
2022-06-04 17:56:03 +0300

    Create Contacts component

--- client/src/pages/contacts/contacts.jsx
+++ client/src/pages/contacts/contacts.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Navigation } from "../../ui/navigation/navigation";
+import { Headerpage } from "../../components/header-page/header-page";
+import contacts from "./img/icon-contacts.svg";
 import s from "./contacts.module.css";
 
-export const Contacts = () => {
+export const Contacts = ({ caption }) => {
   return (
-    <div className={s.contacts}>
-      <Navigation />
-    </div>
+    <>
+      <Headerpage picture={contacts} />
+      <main className={s.main}>
+        <h2>{caption}</h2>
+      </main>
+    </>
   );
 };
