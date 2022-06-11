c74b7d701cad6debe2543fa02e5a4a5f3f476557
Anatoly <anatolykostarev@gmail.com>
2022-06-11 21:11:10 +0300

    Refactor Navigation component

--- client/src/components/navigation/navigation.jsx
+++ client/src/components/navigation/navigation.jsx
@@ -9,31 +9,33 @@ import s from "./navigation.module.css";
 
 export const Navigation = () => {
   return (
-    <nav className={s.nav}>
-      <div className={s.items}>
-        <Link to="/platform">
-          <Label picture={smile} alt="smile" />
-          <p>О платформе</p>
-        </Link>
-      </div>
-      <div className={s.items}>
-        <Link to="/signin">
-          <Label picture={pen} alt="pen" />
-          <p>Регистрация</p>
-        </Link>
-      </div>
-      <div className={s.items}>
-        <Link to="login">
-          <Label picture={login} alt="login" />
-          <p>Войти</p>
-        </Link>
-      </div>
-      <div className={s.items}>
-        <Link to="/contacts">
-          <Label picture={contacts} alt="contacts" />
-          <p>Контакты</p>
-        </Link>
-      </div>
-    </nav>
+    <>
+      <nav className={s.nav}>
+        <div className={s.items}>
+          <Link to="/platform">
+            <Label picture={smile} alt="smile" />
+            <p>О платформе</p>
+          </Link>
+        </div>
+        <div className={s.items}>
+          <Link to="/signin">
+            <Label picture={pen} alt="pen" />
+            <p>Регистрация</p>
+          </Link>
+        </div>
+        <div className={s.items}>
+          <Link to="login">
+            <Label picture={login} alt="login" />
+            <p>Войти</p>
+          </Link>
+        </div>
+        <div className={s.items}>
+          <Link to="/contacts">
+            <Label picture={contacts} alt="contacts" />
+            <p>Контакты</p>
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 };
