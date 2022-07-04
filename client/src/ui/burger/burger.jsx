2bfc71c1e89a4740c738a1a1d156817e8b523640
Anatoly <anatolykostarev@gmail.com>
2022-07-04 16:03:30 +0300

    Add props fot UI Burger

--- client/src/ui/burger/burger.jsx
+++ client/src/ui/burger/burger.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import burger from "./icon-burger.svg";
 import s from "./burger.module.css";
 
-export const Burger = () => {
+export const Burger = ({ onClick }) => {
   return (
-    <div className={s.burger}>
+    <div className={s.burger} onClick={onClick}>
       <img src={burger} alt="burger" />
     </div>
   );
